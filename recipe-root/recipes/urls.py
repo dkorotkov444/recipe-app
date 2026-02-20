98e7d698e2b8a8148680a58478a5c194b9121b1d
DK-CF <korotkovadvisory@gmail.com>
2026-02-20 07:25:39 +0100

    A2-E2.5 Created recipe-list.html web page, RecipeListView, registered them and connected to the homepage.

--- recipe-root/recipes/urls.py
+++ recipe-root/recipes/urls.py
@@ -1,9 +1,11 @@
 # recipes/urls.py
 from django.urls import path
-from .views import home
+from .views import home, RecipesListView, RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('recipes/', RecipesListView.as_view(), name='recipes_list'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
--- recipe-root/recipes/views.py
+++ recipe-root/recipes/views.py
@@ -1,7 +1,19 @@
 # recipes/views.py
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView      # Import ListView and DetailView for class-based views
+from .models import Recipe
 
-# Create your views here.
 # Homepage view
 def home(request):
     return render(request, 'recipes/recipes_home.html')
+
+# Class-based views for listing recipes and showing recipe details
+class RecipesListView(ListView):          # class-based view to display a list of recipes
+    model = Recipe                        # specify the model to use for this view
+    template_name = 'recipes/recipes_list.html'   # specify the template to render
+    context_object_name = 'recipes'       # specify the context variable name to use in the template
+
+class RecipeDetailView(DetailView):      # class-based view to display details of a single recipe
+    model = Recipe                        # specify the model to use for this view
+    template_name = 'recipes/recipe_detail.html' # specify the template to render
+    context_object_name = 'recipe'        # specify the context variable name to use in the template
