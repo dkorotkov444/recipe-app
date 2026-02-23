e234a61bfa488d03448eeec1ce14630a569e8037
DK-CF <korotkovadvisory@gmail.com>
2026-02-23 19:33:36 +0100

    A2-E2.7 Fixed recipes_search.html errors and registered paths.

--- recipe-root/recipes/urls.py
+++ recipe-root/recipes/urls.py
@@ -1,12 +1,16 @@
 # recipes/urls.py
 from django.urls import path
-from .views import RecipesListView, RecipeDetailView
+from .views import RecipesListView, RecipeDetailView, records
 
 app_name = 'recipes'
 
 urlpatterns = [
     # This becomes: http://127.0.0.1:8000/recipes/list/
     path('list/', RecipesListView.as_view(), name='recipes_list'),
+    
     # This becomes: http://127.0.0.1:8000/recipes/recipe/<id>/
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+
+    # Data Lab / Search view. This becomes: http://127.0.0.1:8000/recipes/search/
+    path('search/', records, name='recipes_search'),
 ]
